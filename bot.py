9dd43f0013aa18179b519540524ef4400b307095
mehmeteminibis <ibismehmetemin1@gmail.com>
2025-11-30 02:19:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -485,7 +485,7 @@ def handle_civciv_satin_alma(message):
     user_id = message.from_user.id
     text = message.text
     data, user_id_str = get_user_data(user_id)
-    current_civciv_count = len([c for c in data[user_id_str]['civciv_list'] if c['status'] == 'civciv'])
+    if current_civciv_count >= MAX_CIVCIV_OR_TAVUK:
     
     civciv_color = text.replace('💰 Satın Al: ', '')
     
@@ -918,6 +918,7 @@ if __name__ == '__main__':
         print(f"Bot Çalışma Hatası: {e}. 5 saniye sonra yeniden deneniyor.")
 
         time.sleep(5)
+
 
 
 
